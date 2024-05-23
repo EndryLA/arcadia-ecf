9c7268615dfb8693a098c0aa8dbbdc9a1e427026
Endry LUNDY ANDRES <clansdoli@gmail.com>
2024-05-23 21:15:05 +0200

    changed index.php to root

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -5,3 +5,11 @@ try {
 } catch (PDOException $error) {
     echo '<h1>' . 'Une erreur est survenue' .'</h1>' ;
 }
+
+
+$db_host = '';
+$db_user = '';
+$db_pass = '';
+$db_name = '';
+
+$pdo2 = new PDO('mysql:dbname='. $db_name . ';host=' . $db_host,$db_user,$db_pass);
