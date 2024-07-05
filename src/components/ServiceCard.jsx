fa01e50de15e6804deec5e5819f4a341cc4792d6
Endry LUNDY ANDRES <clansdoli@gmail.com>
2024-07-05 15:00:42 +0200

    ajout de 2 sections à la homepage

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,13 @@
+function ServiceCard({title,content}) {
+        return(
+        <div className="card card-service">
+            <h3>{title}</h3>
+            <p>{content}</p>
+        </div>
+        )
+}
+
+
+
+
+export default ServiceCard
