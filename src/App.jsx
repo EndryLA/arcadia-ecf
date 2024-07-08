202a8aa2a734a1c3a64dcf22112fac63048162fd
EndryLA <117721438+EndryLA@users.noreply.github.com>
2024-07-08 10:55:43 +0200

    Revert "Dev"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import HomePage from './components/HomePage'
-import LoginPage from './components/LoginPage'
-import Header from './components/Header'
 import './styles.css'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App () {
   return (
-    <>
-    <Header />
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/connexion' element={<LoginPage/>}/>
-      </Routes>
-
-    </Router>
-    </>
+    <HomePage />
   )
 }
 export default App
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,5 @@
 import Header from './Header'
-import ServiceCard from './ServiceCard'
 import HeroImage from '@assets/hero-image.jpg'
-import LionImage from '@assets/lion.jpg'
-import HibouImage from '@assets/hibou.jpg'
-import PerroquetImage from '@assets/perroquet.jpg'
 
 function Hero() {
     return (
@@ -18,68 +14,11 @@ function Hero() {
     )
 }
 
-function Habitats() {
-    return (
-        
-        <section className='habitats-section'>
-            <div className='habitat'>
-                <img src={LionImage}/>
-                <div className='habitat-info'>
-                    <h3>La jungle du roi</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nulla est quos quibusdam natus vitae minus sit. Facere distinctio natus magni qui nisi! Optio eius veritatis voluptatibus reprehenderit maxime? Nesciunt deleniti facilis eaque maiores alias, doloribus nulla provident praesentium!</p>
-                    <a className='button' href=''>Visiter</a>
-                </div>
-             </div> 
-             <div className='habitat habitat-reverse'>
-                <img src={PerroquetImage}/>
-                <div className='habitat-info'>
-                    <h3>La cage aux oiseaux</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nulla est quos quibusdam natus vitae minus sit. Facere distinctio natus magni qui nisi! Optio eius veritatis voluptatibus reprehenderit maxime? Nesciunt deleniti facilis eaque maiores alias, doloribus nulla provident praesentium!</p>
-                    <a className='button' href=''>Visiter</a>
-                </div>
-             </div> 
-
-             <div className='habitat'>
-                <img src={HibouImage}/>
-                <div className='habitat-info'>
-                    <h3>Singes en folie</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quidem nulla est quos quibusdam natus vitae minus sit. Facere distinctio natus magni qui nisi! Optio eius veritatis voluptatibus reprehenderit maxime? Nesciunt deleniti facilis eaque maiores alias, doloribus nulla provident praesentium!</p>
-                    <a className='button' href=''>Visiter</a>
-                </div>
-             </div>  
-        </section>
-        
-    )
-}
-
-function HomePageServices() {
-    return (
-        <>
-        <h2>Nos Services</h2>
-        <div className='card-container'>
-            <ServiceCard 
-            title='Tour du zoo en petit train' 
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-            />
-            <ServiceCard 
-            title='Lorem Ipsum' 
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-            />
-            <ServiceCard 
-            title='Lorem Ipsum' 
-            content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
-            />
-        </div>
-        </>
-    )
-}
-
 function HomePage() {
     return (
         <>
+        <Header />
         <Hero />
-        <Habitats />
-        <HomePageServices />
 
         </>
     )
